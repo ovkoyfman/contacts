27d39103e2f9fc8a4374050c0c386e3d6aeb643d
Oksana Koyfman <oksanakoyfman@oksanas-mbp.home>
2021-06-06 23:55:58 -0400

    Updated ListContacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -3,8 +3,17 @@ class ListContacts extends Component{
     render(){
         return(
             <ol className='contact-list'>
-                {this.props.contacts.map(contact => <li key={contact.id}>{contact.name}</li> 
-                )}
+                {this.props.contacts.map(contact => (
+                <li className='contact-list-item' key={contact.id}>
+                    <div className='contact-avatar' style={{backgroundImage:`url(${contact.avatarURL})`}}></div>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button className='contact-remove'>Remove</button>
+                </li> 
+                
+                ))}
             </ol>
         )  
     }
